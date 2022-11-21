c26834cfb393253165ed6e1da6faffe62a3b388c
Marker97 <chereche10@gmail.com>
2022-11-21 17:14:49 +0100

    VariablesAndCastingBetweenTypes

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val number1: Long = 4
+    val number2: Double = 4.0
+    val number3: Short = 2
+    val sum = number1 + number2 + number3
+
+    println("Number sum: $sum")
+}
